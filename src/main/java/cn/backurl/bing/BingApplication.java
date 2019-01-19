27f07642c315c69d1838ea7b92bff7ef71c7381a
akid <1026705223@qq.com>
2019-01-19 14:33:11 +0800

    创建（bing）：必应壁纸项目立项

---
+++ src/main/java/cn/backurl/bing/BingApplication.java
@@ -0,0 +1,14 @@
+package cn.backurl.bing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BingApplication.class, args);
+	}
+
+}
+
