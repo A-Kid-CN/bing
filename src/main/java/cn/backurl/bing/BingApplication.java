aa77b93db575c1eb8ba614c2c5abaa00b47ca399
akid <1026705223@qq.com>
2019-04-11 00:09:59 +0800

    添加：添加几个module

--- src/main/java/cn/backurl/bing/BingApplication.java
+++ src/main/java/cn/backurl/bing/BingApplication.java
@@ -2,12 +2,17 @@ package cn.backurl.bing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.ComponentScan;
 
+@EnableEurekaServer
 @SpringBootApplication
+@ComponentScan(basePackages = {"cn.backurl.bing.redis"})
 public class BingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BingApplication.class, args);
+
 	}
 
 }
