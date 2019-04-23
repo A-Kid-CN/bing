1f821873405695364e25122e51b72f6d714d5e62
akid <1026705223@qq.com>
2019-04-23 23:49:48 +0800

    add:迁移代码 add:虚拟路径配置

--- bing-wallpaper/src/main/java/cn/backurl/bing/WebApplication.java
+++ bing-wallpaper/src/main/java/cn/backurl/bing/WebApplication.java
@@ -1,17 +1,29 @@
 package cn.backurl.bing;
 
+import lombok.extern.log4j.Log4j2;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(scanBasePackages = {"cn.backurl.bing"})
 @MapperScan("cn.backurl.bing.dao.*")
-public class WebApplication {
+@Log4j2
+public class WebApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
+		log.info("spring启动完毕");
+	}
 
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/image/**").addResourceLocations("file:/usr/local/bing/");
+
+		log.info("注入文件虚拟路径");
 	}
 
 }
--- bing-wallpaper/src/main/java/cn/backurl/bing/controller/TranferDataController.java
+++ bing-wallpaper/src/main/java/cn/backurl/bing/controller/TranferDataController.java
@@ -2,9 +2,11 @@ package cn.backurl.bing.controller;
 
 import cn.backurl.bing.dao.wallpaper.WallpaperMapper;
 import cn.backurl.bing.dao.wallpaper.WallpaperService;
+import cn.backurl.bing.model.wallpaper.Wallpaper;
 import cn.backurl.bing.result.AjaxResult;
 import cn.backurl.bing.result.ResultCode;
 import freemarker.template.utility.StringUtil;
+import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
+import java.util.Date;
 
 /**
  * <p>
@@ -24,12 +27,12 @@ import java.io.File;
  **/
 @RestController
 @RequestMapping("/tranfer")
-@Slf4j
+@Log4j2
 public class TranferDataController {
 
 
     @Autowired
-    private WallpaperService wallpaperService;
+    private WallpaperMapper wallpaperMapper;
 
     /**
      * <p>
@@ -47,21 +50,45 @@ public class TranferDataController {
         if (StringUtils.isBlank(basePath) || StringUtils.isBlank(year)) {
             return AjaxResult.failure(ResultCode.ParamException);
         }
+        log.info("paramn:{},{}", basePath,year);
         int monthNum = 12;
         if (year.equals(2015)) {
             monthNum = 3;
         }
         for (int i = 12; monthNum > 0; monthNum--, i--) {
             File file = new File(basePath + "/" + year + "年/" + year + "Bing" + (i >= 10 ? i : "0" + i));
-            if (file.isFile() && file.exists()) {
+            log.info("输出当前文件路径"+file.getPath());
+            if (file.exists()) {
                 File[] files = file.listFiles();// 获取目录下的所有文件或文件夹
                 if (files == null) {// 如果目录为空，直接退出
                     continue;
                 }
-                for(File item :files){
-                    log.info(item.getName());
+                Integer day = 1;
+                for (File item : files) {
+                    log.info("输出获取的的文件名"+item.getName());
+                    String name = file.getName();
+                    String path = file.getPath();
+                    String date = year  +  (i >= 10 ? i : "0" + i)+  (day >= 10 ? day : "0" + day);
 
+                    Wallpaper wallpaper = new Wallpaper();
+                    wallpaper.setName(name);
+                    wallpaper.setPath(path);
+                    wallpaper.setMiniPath("");
+                    wallpaper.setClick(0);
+                    wallpaper.setLove(0);
+                    wallpaper.setCopyRight("暂时缺失版权信息");
+                    wallpaper.setDate(date);
+                    wallpaper.setDownload(0);
+                    wallpaper.setGmtCreate(new Date());
+                    wallpaper.setGmtModified(new Date());
+                    wallpaper.setOriginal("");
+
+                    wallpaperMapper.insert(wallpaper);
+                    day++;
                 }
+            } else {
+                log.info("文件不存在");
+                return AjaxResult.failure(ResultCode.ParamException, "文件不存在");
             }
         }
         return AjaxResult.success();
